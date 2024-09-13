9a580ef70a88bb6fb005d62926e764addc222598
Konstantin <kastikus.stixanov@mail.ru>
2024-09-13 11:14:53 +0300

    edit names

--- guess_number.py
+++ guess_number.py
@@ -1,14 +1,14 @@
 from random import randint as rnd
 
 
-number = rnd(0, 5)
+number = rnd(1, 5)
 
-print(f'Угадай-ка, моя попастая Викуля...')
+print(f'Угадай-ка, Викуля...')
 print(f'Число..... от 1 до 5!')
 
 while True:
 
-    guess = int(input(f'Введи число, попастик: '))
+    guess = int(input(f'Введи число, Викулик: '))
 
     if guess < number:
         print(f'Твоё число меньше того, что загадано.')
